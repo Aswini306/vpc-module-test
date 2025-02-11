52dfb1c6883369d6d0eb32c1e30e4497f33a6c4d
Aswini306 <ashupuri5@gmail.com>
2025-02-11 11:41:48 +0530

    vpc

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "81s-remote-state"
+    bucket = "81s-aswini-devops"
     key    = "expense-vpcc"
     region = "us-east-1"
     dynamodb_table = "81s-locking"
